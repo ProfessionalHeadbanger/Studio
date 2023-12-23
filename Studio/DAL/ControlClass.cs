eca5d0ca7f2d5075080df878af34a34aff02bdbd
ProfessionalHeadbanger <nice.sevkabr@mail.ru>
2023-12-23 08:11:55 +0300

    Another fix

--- Studio/DAL/ControlClass.cs
+++ Studio/DAL/ControlClass.cs
@@ -5,69 +5,69 @@ using Studio.DAL;
 
 namespace Studio.DAL
 {
-	public interface IRepository<T> where T : class
-	{
-		IQueryable<T> GetAll();
-		T GetById(object id);
-		void Insert(T entity);
-		void Update(T entity);
-		void Delete(object id);
-		void Save();
-	}
+    public interface IRepository<T> where T : class
+    {
+        IQueryable<T> GetAll();
+        T GetById(object id);
+        void Insert(T entity);
+        void Update(T entity);
+        void Delete(object id);
+        void Save();
+    }
 
-	public class ControlClass<T> : IRepository<T> where T : class
-	{
-		private readonly StudioContext _context;
+    public class ControlClass<T> : IRepository<T> where T : class
+    {
+        private readonly StudioContext _context;
 
-		public ControlClass(StudioContext context)
-		{
-			_context = context ?? throw new ArgumentNullException(nameof(context));
-		}
+        public ControlClass(StudioContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
 
-		public IQueryable<T> GetAll()
-		{
-			return _context.Set<T>();
-		}
+        public IQueryable<T> GetAll()
+        {
+            return _context.Set<T>();
+        }
 
-		public T GetById(object id)
-		{
-			return _context.Set<T>().Find(id);
-		}
+        public T GetById(object id)
+        {
+            return _context.Set<T>().Find(id);
+        }
 
-		public void Insert(T entity)
-		{
-			_context.Set<T>().Add(entity);
-		}
+        public void Insert(T entity)
+        {
+            _context.Set<T>().Add(entity);
+        }
 
-		public void Update(T entity)
-		{
-			_context.Entry(entity).State = EntityState.Modified;
-		}
+        public void Update(T entity)
+        {
+            _context.Entry(entity).State = EntityState.Modified;
+        }
 
-		public void Delete(object id)
-		{
-			T entityToDelete = _context.Set<T>().Find(id);
-			if (entityToDelete != null)
-				_context.Set<T>().Remove(entityToDelete);
-		}
+        public void Delete(object id)
+        {
+            T entityToDelete = _context.Set<T>().Find(id);
+            if (entityToDelete != null)
+                _context.Set<T>().Remove(entityToDelete);
+        }
 
-		public void Save()
-		{
-			_context.SaveChanges();
-		}
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
 
-		public void Dispose()
-		{
-			Dispose(true);
-			GC.SuppressFinalize(this);
-		}
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
 
-		protected virtual void Dispose(bool disposing)
-		{
-			if (disposing)
-			{
-				_context.Dispose();
-			}
-		}
-	}
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _context.Dispose();
+            }
+        }
+    }
 }
